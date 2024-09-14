20a629c7e246ceb94f0d3e98664ff742b79b11ee
Robert Shahla <robertshahla@Roberts-MacBook-Pro.local>
2024-09-14 23:50:57 +0300

    added more fields to csv

--- trace-handler/pcap_to_csv.py
+++ trace-handler/pcap_to_csv.py
@@ -9,7 +9,7 @@ def main():
     while len(files) > 0:
         num_of_files = min(parallel_processes, len(files))
         # requests = [f"""tshark -r {file} -R quic -2 -T fields -e frame.number -e frame.time_relative -e frame.len -e eth.src -e eth.dst -e ip.src -e ip.dst -e ip.proto -e _ws.col.Info -E header=y -E separator=, -E quote=d -E occurrence=f -o tls.keylog_file:$SSLKEYLOGFILE > {file[:-5]}.csv""" for file in files[:num_of_files]]
-        requests = [f"""tshark -r {file} -R quic -2 -T fields -e frame.number -e frame.time_relative -e frame.len -e eth.src -e eth.dst -e ip.src -e ip.dst -e ip.proto -e _ws.col.Info -E header=y -E separator=, -E quote=d -E occurrence=f -o tls.keylog_file:{file.replace('.pcap', '.key')} > {file[:-5]}.csv""" for file in files[:num_of_files]]
+        requests = [f"""tshark -r {file} -R quic -2 -T fields -e frame.number -e frame.time_relative -e frame.len -e eth.src -e eth.dst -e ip.src -e ip.dst -e ipv6.src - e ipv6.dst -e ip.proto -e _ws.col.Info -E header=y -E separator=, -E quote=d -E occurrence=f -o tls.keylog_file:{file.replace('.pcap', '.key')} > {file[:-5]}.csv""" for file in files[:num_of_files]]
         print(len(files))
         processes = [subprocess.Popen(request, shell=True, executable='/bin/bash') for request in requests]
         for process in processes:
